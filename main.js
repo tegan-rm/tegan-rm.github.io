c15228e174d9ba949aa82e403f4d611769cc1fdd
tegan-rm <tegan.roberts.morgan@gmail.com>
2022-07-16 08:27:06 +0100

    Profile picture added

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-function isScrolledIntoView(elem) {
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
-  var elemTop = $(elem).offset().top;
-  return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-}
+
