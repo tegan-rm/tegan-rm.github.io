9e4cf6fcb473241959c0982e7b0fee149ec67df9
tegan-rm <tegan.roberts.morgan@gmail.com>
2022-09-17 01:46:13 +0100

    More bubbles

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,8 +3,22 @@ import "./CSSFiles/HomeCSS.css";
 
 function Home() {
     return (
-        <div><h1>Welcome to my website</h1></div>
+        <div><h1>Welcome to my website</h1>
+        <span className="bubPurple a "></span>
+        <span className="bubYellow b "></span>
+        <span className="bubPurple c "></span>
+        <span className="bubYellow d"></span>
+        <span className="bubPurple e"></span>
+        <span className="bubYellow f"></span>
+        <span className="bubPurple g"></span>
+        <span className="bubYellow h"></span>
+        <span className="bubPurple i"></span>
+        <span className="bubPurple j "></span>
+        <span className="bubYellow k"></span>
+    </div>
     )
 }
 
 export default Home;
+/*
+        */
