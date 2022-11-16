2635bcff48a3dfddc3bc9fc01e378c67a82f81f5
tegan-rm <tegan.roberts.morgan@gmail.com>
2022-11-16 21:53:28 +0000

    Navbar fixed

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import styles from './Nav.module.css';
+import './NavBarCSS.css';
 import logo from './Pictures/Logo.png';
 import {Link} from "react-router-dom";
 
 function NavBar(){
     return (
-        <div className={styles.navbar} >
-        
-            <img src={logo} alt="logo" width="100" height="100" />
-            <div className={styles['logo-container']}>
-                <span>Tegan Roberts-Morgan</span>
-            </div>
-            <nav className={styles['links-container']}>
-                <div className={styles['link']}>
-                <Link to={"/"}>Home</Link> 
+        <div className='navbar' >              
+            <img src={logo} alt="logo" className='logoImage' />
+            <div className ="navbarText">
+                <div className='logoText'>
+                    <span>Tegan Roberts-Morgan</span>
                 </div>
-                <div className={styles['link']}>
-                <Link to={"/about"}>About</Link>
-                </div>
-                <div className={styles['link']}>
-                <Link to={"/contact"}>Contact</Link>
-                </div>
-                <div className={styles['link']}>
-                <Link to={"/projects"}>Projects</Link>
-                </div>
-                
-        </nav>
+                <nav className='linksContainer'>
+                    <div className='link'>
+                        <Link to={"/"}>Home</Link> 
+                    </div>
+                    <div className='link'>
+                        <Link to={"/about"}>About</Link>
+                    </div>
+                    <div className='link'>
+                        <Link to={"/contact"}>Contact</Link>
+                    </div>
+                    <div className='link'>
+                        <Link to={"/projects"}>Projects</Link>
+                    </div>                
+                </nav>
+             </div>
         </div>
     
 )}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CSSFiles/HomeCSS.css";
-import sunflower from '../Pictures/Sunflower1.png';
+import sunflower from '../Pictures/Sunflower.png';
 import lavender from '../Pictures/Lavender.png';
 
 function Home() {
@@ -11,17 +11,12 @@ function Home() {
             </head>
             <div>
                 <div  className="text">
-                    <h1>Hi, I'm Tegan</h1>
-                    <h3>I am currently a 3rd Year Computer Science Student studying at the University of Bristol </h3>
+                    <h1 className="header" >Hello, I'm Tegan</h1>
+                    <h3 className="introduction" >I am currently a 3rd Year Computer Science Student studying at the University of Bristol </h3>
                 </div>
 
                 <div className ="flower">
-                    <div id="sliderSunflower1" className="slide-in">
-                        <img src={sunflower} alt="logo" width="100" height="100" />
-                    </div>
-                    <div id="sliderLavender1" className="slide-in">
-                        <img src={lavender} alt="logo" width="100" height="100" />
-                    </div>
+                        <img src={sunflower} className="sunflowerImage" />
                 </div>
 
             </div>
